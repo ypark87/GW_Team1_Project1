aa18f4c8ac86454bef899760c75cfdcb564c96d6
elenastaylor <elena.staylor@gmail.com>
2017-04-04 00:57:19 -0400

    updated app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,7 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-/*weeklyTotal = function(first){
-
-}*/
-
+//declare a function to check that number entered is valid
 function getIntValue(valtoCheck) {
     var result = parseInt(valtoCheck);
     if (isNaN(result)) {
@@ -26,38 +23,49 @@ function getIntValue(valtoCheck) {
     return result;
 }
 
+//declare table variables and set their values at zero
 var newWeek2 = {
     goal: 0,
     monday: 0,
     tuesday: 0,
     wednesday: 0,
-
-    totalMin: /**weeklyTotal**/0
+    thursday: 0,
+    friday: 0,
+    saturday: 0,
+    sunday: 0,
+    totalMin: 0
 };
 
 //Variables
 $("#addWeek").on("click", function() {
     //alert("test");
     //User Input
-    var weeklyGoal = $('#weeklyGoal').val().trim();
-    var weekday1 = $('#weekday1').val().trim();
-    var weekday2 = $('#weekday2').val().trim();
-    var weekday3 = $('#weekday3').val().trim();
-    var weekday4 = $('#weekday4').val().trim();
-    var weekday5 = $('#weekday5').val().trim();
-    var weekday6 = $('#weekday6').val().trim();
-    var weekday7 = $('#weekday7').val().trim();
+    var weeklyGoal = getIntValue($('#weeklyGoal').val().trim());
+    var weekday1 = getIntValue($('#weekday1').val().trim());
+    var weekday2 = getIntValue($('#weekday2').val().trim());
+    var weekday3 = getIntValue($('#weekday3').val().trim());
+    var weekday4 = getIntValue($('#weekday4').val().trim());
+    var weekday5 = getIntValue($('#weekday5').val().trim());
+    var weekday6 = getIntValue($('#weekday6').val().trim());
+    var weekday7 = getIntValue($('#weekday7').val().trim());
+    //var total = $('#total').val().trim();
 
 
     newWeek2.goal = weeklyGoal;
     newWeek2.monday = weekday1;
     newWeek2.tuesday = weekday2;
     newWeek2.wednesday = weekday3;
+    newWeek2.thursday = weekday4;
+    newWeek2.friday = weekday5;
+    newWeek2.saturday = weekday6;
+    newWeek2.sunday = weekday7;
+    //newWeek2.total = totalMin;
 
 
 
-    var sum = getIntValue(weekday1) + getIntValue(weekday2) + getIntValue(weekday3) + getIntValue(weekday3) + getIntValue(weekday4) +
-        getIntValue(weekday5) + getIntValue(weekday6) + getIntValue(weekday7);
+
+    var sum = weekday1 + weekday2 + weekday3 + weekday3 + weekday4 +
+        weekday5 + weekday6 + weekday7;
 
     console.log("weekly Goal: " + weeklyGoal);
     console.log("week day 1:" + weekday1);
@@ -83,7 +91,8 @@ $("#addWeek").on("click", function() {
 //Clear off the text-boxes
     //$("#weekList").append("<tr><td>" + newWeek2.goal+ "</td><td>" + newWeek2.totalMin +"</td></tr>");
 
-    $("#weekList").append("<tr><td>" + newWeek2.goal+ "</td><td>" + newWeek2.monday +"</td><td>" + newWeek2.tuesday +"</td><td>" + newWeek2.wednesday +"</td></tr>");
+    $("#weekList").append("<tr><td >" + newWeek2.goal+ "</td><td>" + newWeek2.monday +"</td><td>" + newWeek2.tuesday +"</td><td>" + newWeek2.wednesday +"</td><td>" +
+         + newWeek2.thursday + "</td><td>" + newWeek2.friday + "</td><td>" + newWeek2.saturday + "</td><td>" + newWeek2.sunday + "</td><td>" + sum + "</td></tr>");
 
     $("#weeklyGoal").val("");
     $("#weekday1").val("");
@@ -93,30 +102,50 @@ $("#addWeek").on("click", function() {
     $("#weekday5").val("");
     $("#weekday6").val("");
     $("#weekday7").val("");
+    $("#totalMin").val("");
 
     return false;
 
 });
+// Circle Bar Tracker
+var bar = new ProgressBar.Circle(container, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#333', width: 4 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
 
-/*database.ref().on("child_added", function(snapshot, prevChildKey) {*/
-   /* var newWeek = snapshot.val();*/
-   /* console.log("Previous Post ID: " + prevChildKey);*/
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
 
-/*var weeklyTotal = 0;
-    for(var i = 0; i < weeklyTotal.length; i++){
-        if(parseInt(arr[i].innerHTML))
-            weeklyTotal+= parseInt(arr[i].innerHTML);
     }
-    console.log(weeklyTotal);*/
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
 
-   /* $("#weekList").append("<tr><td>" + newWeek2.goal+ "</td><td>" + newWeek2.totalMin +"</td></tr>");
+bar.animate(1.0);
 
-    $("#weeklyGoal").val("");
-    $("#weekday1").val("");
-    $("#weekday2").val("");
-    $("#weekday3").val("");
-    $("#weekday4").val("");
-    $("#weekday5").val("");
-    $("#weekday6").val("");
-    $("#weekday7").val("");
-});*/
+//link to the progress bar/progress circle
+function progressBar(value) {
+    var weeklyGoal = 0;
+    var weeklyTotal = 0;
+     result(weeklyTotal/weeklyGoal*100);
+}
+/*database.ref().on("child_added", function(snapshot, prevChildKey) {*/
+/* var newWeek = snapshot.val();*/
+/* console.log("Previous Post ID: " + prevChildKey);*/
